9fb0eb6f034ac8af351bb51e68b27622044fe4ea
jacobrmcadam <jacobrmcadam@gmail.com>
2018-10-12 13:03:52 -0700

    Progress
    Rearranged things, new project files, etc.

---
+++ wip/v4/theme.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+//    $("#main a").before( $(
+//    
+//      "<i class='material-icons'>link</i>"
+//    
+//    ) );
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
